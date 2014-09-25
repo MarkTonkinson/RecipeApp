8c3f2836ab68faddbad58532cc234f3f933cff55
Mark Tonkinson <DHTMaster@gmail.com>
2014-09-25 01:19:44 -0600

    First Commit
    First Commit

---
+++ TestingRecipeMonkey/RMbetajs/rmbeta.js
@@ -0,0 +1,19 @@
+/*var submitIngredient = function(){
+     var firstIngredient = document.getElementById("ingredient-one");
+     var alertIngredient = function(firstIngredient){
+        alert(firstIngredient);
+    }  
+    if("ingredient-one-name".value !== 0)
+        document.getElementById();
+          
+  
+};
+
+//build a constructor function for ingredients
+//this.ingredientName
+//this.number
+
+
+
+
+//]; */
