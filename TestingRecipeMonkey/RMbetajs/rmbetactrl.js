e6ef674b11985a4a9eb5150e328767b18d713b3c
Mark Tonkinson <DHTMaster@gmail.com>
2014-10-16 22:39:18 -0600

    Just running a few tests on how to start thinking about data storage and writing search algorithms, plus added some hard coded data to play around with

--- TestingRecipeMonkey/RMbetajs/rmbetactrl.js
+++ TestingRecipeMonkey/RMbetajs/rmbetactrl.js
@@ -2,4 +2,8 @@ var recipeMonkey = angular.module('recipeMonkey');
 
 recipeMonkey.controller('rmbetactrl', function($scope, rmbetaservice){
 	$scope.test = "hello world";
+
+	$scope.recipes = rmbetaservice.getRecipeArray();
+
+	$scope.newRecipe;
 });
--- TestingRecipeMonkey/RMbetajs/rmbetaservice.js
+++ TestingRecipeMonkey/RMbetajs/rmbetaservice.js
@@ -1,5 +1,27 @@
 var recipeMonkey = angular.module('recipeMonkey');
 
 recipeMonkey.service('rmbetaservice', function(){
+
 	
+
+	var Recipe = function(recipeName, recipeIngredients, recipeAuthor, recipeYield, recipeLocation) {
+	   this.recipeName = recipeName;
+	   this.recipeIngredients = recipeIngredients;
+	   this.recipeAuthor = recipeAuthor;
+	   this.recipeYield = recipeYield;
+	   this.recipeLocation = recipeLocation;
+	}
+
+	var myPizzaSauce = new Recipe('Marks Pizza Sauce', ['lemon juice', 'tomato paste', 'balsamic vinegar', 'brown sugar', 'butter', 'thyme', 'oregano', 'rosemary', 'milk'], 'Mark', '2', 'Recipe Book');
+
+	var WhitneysChicken = new Recipe('Whitneys Chicken', ['chicken', 'apricot jam', 'pineapple', 'onion', 'red pepper'], 'Whitney', '4', 'website');
+
+	var simplePizzaSauce = new Recipe('Simple Pizza Sauce', ['tomato paste', 'balsamic vinegar', 'butter', 'thyme', 'oregano'], 'Mark', '5', 'website');
+	
+
+	var recipeArray = [myPizzaSauce, WhitneysChicken, simplePizzaSauce];
+
+	this.getRecipeArray = function(){
+		return recipeArray;
+	}
 })
